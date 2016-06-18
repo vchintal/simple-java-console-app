f359b975552cce0759e5ce192f1dd17171c5e017
vchintal <Vijay.Chintalapati@gmail.com>
2016-06-17 23:59:38 -0700

    Cleaned up long tab spaces

--- src/main/java/org/everythingjboss/console/HelloWorld.java
+++ src/main/java/org/everythingjboss/console/HelloWorld.java
@@ -5,10 +5,10 @@ import org.slf4j.LoggerFactory;
 
 public class HelloWorld {
 
-	private static final Logger logger = LoggerFactory.getLogger(HelloWorld.class);
-	
-	public static void main(String[] args) {
-		logger.info("Hello World!");
-	}
+    private static final Logger logger = LoggerFactory.getLogger(HelloWorld.class);
+    
+    public static void main(String[] args) {
+        logger.info("Hello World!");
+    }
 
 }
